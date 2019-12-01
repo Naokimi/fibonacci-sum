929cf41145844eeaac7f4c2551cdfd7d9ad1eb4d
Paulo DAlberti <phdalberti@gmail.com>
2019-12-01 12:15:25 +0900

    finished tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,1 +1,12 @@
 require "app"
+
+describe "fibonacci_sum" do
+
+  it "should return the correct sum" do
+    expect(fibonacci_sum(6)).to eq 12
+  end
+
+  it "should return -1 if max is smaller than 1" do
+    expect(fibonacci_sum(-1)).to eq -1
+  end
+end
